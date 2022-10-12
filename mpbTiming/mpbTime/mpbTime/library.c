a2f65499bf6bf2b6e978bf848074099eabd7f2fa
Juan-Andres-Castano <comerciokits@yahoo.com>
2022-10-12 13:47:07 -0400

    BASELINE FOR TIMER - samx8

---
+++ mpbTiming/mpbTime/mpbTime/library.c
@@ -0,0 +1,17 @@
+/*
+ * mpbTime.c
+ *
+ * Created: 2022-09-27 6:03:25 AM
+ * Author : comer
+ */ 
+
+#include "sam.h"
+#include "mpbTimeBase.h"
+
+mpbTimeBase_t mpbTimeBase;
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
